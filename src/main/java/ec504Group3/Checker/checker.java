ce3c26b54de7ded17414545bd55f710c3dee4650
Ting Zhang <ztvince@bu.edu>
2020-04-11 18:04:32 -0400

    new

---
+++ src/main/java/ec504Group3/Checker/checker.java
@@ -0,0 +1,9 @@
+package ec504Group3.Checker;
+
+public interface checker {
+    /**
+     * @argument File address of file needed to check
+     * @return Score for specific  File
+     */
+    int check(String checkFile);
+}
