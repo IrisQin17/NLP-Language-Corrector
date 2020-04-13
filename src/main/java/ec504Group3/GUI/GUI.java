91d9a5a0195bb9ef523308565870f7285a3d522d
Yuan Wei <yuanwei@bu.edu>
2020-04-12 21:17:26 -0400

    Basic GUI fix

--- src/main/java/ec504Group3/GUI/GUI.java
+++ src/main/java/ec504Group3/GUI/GUI.java
@@ -3,26 +3,36 @@ package ec504Group3.GUI;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
 
 public class GUI {
     private JPanel panel1;
     private JTextArea textField;
     private JRadioButton EnglishButton;
-    private JFormattedTextField CommandInput;
+    private JFormattedTextField url_Input;
     private JButton enterButton;
     private JTextArea Tittle;
     private JRadioButton ChineseButton;
     private JRadioButton FrenchButton;
+    private JButton resetButton;
+    private JFormattedTextField test_Input;
     private boolean EnglishSelected, ChineseSelected, FrenchSelected = false;
+    public String crawlerInput ="URL-list";
+    public String checkerInput;
 
     public GUI() {
         enterButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String inputCom = CommandInput.getText();
+                if(url_Input.getText().equals(null) || url_Input.getText().equals("") ||test_Input.getText().equals(null) || test_Input.getText().equals("") ){
+                    textField.setText("plz filling both input");
+                    return;
+                }
+                String inputCom = url_Input.getText();
+                crawlerInput = url_Input.getText();
                 textField.setText(inputCom);
-
+                inputCom += "\n"+test_Input.getText();
+                checkerInput = test_Input.getText();
+                textField.setText(inputCom);
             }
         });
 
@@ -50,10 +60,28 @@ public class GUI {
                 }
             }
         });
+
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                url_Input.setText(null);
+                textField.setText(null);
+            }
+        });
+
     }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    // 解析
+    private void commandAnalyzer(String inputStr){
+        String[] list = inputStr.split(" ");
+        for (String s : list){
+            System.out.println(" " + s);
+        }
+        return;
     }
 
     public static void main(String[] args) {
@@ -62,5 +90,8 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+//        GUI myGUI = new GUI();
+//        String test = "crawler www.google.com";
+//        myGUI.commandAnalyzer(test);
     }
 }
